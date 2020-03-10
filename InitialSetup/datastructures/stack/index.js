a701a3c42c3a0077f294b9efcc1c1e96d5aecb94
Nagendra <nagasankoju5687@gmail.com>
2020-03-11 01:17:45 +0530

    Stacks

---
+++ InitialSetup/datastructures/stack/index.js
@@ -0,0 +1,49 @@
+/**
+ * Stack
+ */
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;        
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;       
+    }
+    push(val) {
+        var newNode = new Node(val);
+        if(!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return this.size++;
+    }
+
+    pop() {
+        if(!this.first) return null;
+        var temp = this.first; 
+        if(this.first === this.last) {
+            this.last = null;
+        };
+        this.first = this.first.next;
+        this.size--;
+        return temp.val;
+    }
+    
+}
+
+var stack = new Stack();
+stack.push(5);
+stack.push(4);
+stack.push(3);
+stack.push(2);
+console.log(stack.push(1));
+console.log(stack);
