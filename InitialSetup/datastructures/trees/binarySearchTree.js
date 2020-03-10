a26a202db8877e53a2461137a36859be6093d46f
Nagendra <nagasankoju5687@gmail.com>
2020-03-11 03:02:27 +0530

    Insert - BINARY SEARCH TREE

---
+++ InitialSetup/datastructures/trees/binarySearchTree.js
@@ -0,0 +1,52 @@
+/**
+ * Binary Search Tree
+ */
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null       
+    }
+    
+    insert(val) {
+        var newNode = new Node(val);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        } 
+        var current = this.root;
+        while(true) {
+            if(val === current.val) return undefined;
+            if(val < current.val) {
+                if(current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null) {
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }
+    }
+}
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(9);
+console.log(tree);
