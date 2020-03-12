52f7e9cc85693cc8b3e5a6d804e87e311a28c5ad
Nagendra <nagendra@regaltusk.com>
2020-03-12 16:41:38 +0530

    Graph Traversal

---
+++ InitialSetup/datastructures/graphs/graphTraversal/index.js
@@ -0,0 +1,17 @@
+/**
+ * Graph Traversal
+*/
+
+// Visiting / Updating / Checking
+
+/**
+ * Uses of Graph Traversal:-
+ * 
+ * 1. Peer to Peer Networking
+ * 2. Web Crawlers
+ * 3. Finding "Closest" matches/recommendations
+ * 4. Shortest path problems
+ *      . GPS Navigation
+ *      . Solving mazes
+ *      . AI (Shortest path to win the game)
+*/
